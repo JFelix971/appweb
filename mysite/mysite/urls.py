339090a8e8e99a9eedc1ed2c18aed44b269786da
Jeremy FELIX <jeremy.felix971@gmail.com>
2018-10-03 22:33:55 -0400

    add mysite project ifap and modify polls project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ifap/', include('ifap.urls')),
     path('polls/', include('polls.urls')),
 ]
